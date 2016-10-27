45b800296402657ba007817e831981bb75905b3d
Stéphan Kochen <stephan@kochen.nl>
2016-10-27 15:50:01 +0200

    Add a Redis store.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports = module.exports = require('./lib/client');
 exports.AbstractStore = require('./lib/store');
 exports.MemoryStore = require('./lib/stores/memory');
+exports.RedisStore = require('./lib/stores/redis');
